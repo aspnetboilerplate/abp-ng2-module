50b18bb67c065338bb771babfebe06c83c07a8f1
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-02-13 13:24:45 +0300

    fixed #14 removed abpHttp and implemented abpHttpInterceptor

--- src/abp-user-configuration.service.ts
+++ src/abp-user-configuration.service.ts
@@ -1,19 +1,19 @@
 ﻿import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 declare var jQuery: any;
 
 @Injectable()
 export class AbpUserConfigurationService {
 
-    constructor(private _http: Http) {
+    constructor(private _http: HttpClient) {
         
     }
 
     initialize(): void {
         this._http.get('/AbpUserConfiguration/GetAll')
             .subscribe(result => {
-                jQuery.extend(true, abp, result.json());
+                jQuery.extend(true, abp, JSON.parse(JSON.stringify(result)));
             });
     }
 
--- src/abp.module.ts
+++ src/abp.module.ts
@@ -1,5 +1,6 @@
 ﻿import { NgModule } from '@angular/core';
-import { HttpModule, JsonpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { JsonpModule, XHRBackend, RequestOptions } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AbpSessionService } from './session/abp-session.service';
 import { PermissionCheckerService } from './auth/permission-checker.service';
@@ -10,24 +11,14 @@ import { NotifyService } from './notify/notify.service';
 import { MessageService } from './message/message.service';
 import { LogService } from './log/log.service';
 import { AbpMultiTenancyService } from './multi-tenancy/abp-multi-tenancy.service';
-import { AbpHttpConfiguration, AbpHttp } from './abpHttp';
+import { AbpHttpConfiguration } from './abpHttpInterceptor';
 import { AbpUserConfigurationService } from './abp-user-configuration.service';
 import { TokenService } from './auth/token.service';
 import { UtilsService } from './utils/utils.service';
 
-export function abpHttpFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions, configuration: AbpHttpConfiguration): Http {
-    return new AbpHttp(xhrBackend, requestOptions, configuration);
-}
-
-export let ABP_HTTP_PROVIDER = {
-    provide: Http,
-    useFactory: abpHttpFactory,
-    deps: [XHRBackend, RequestOptions, AbpHttpConfiguration]
-};
-
 @NgModule({
     imports: [
-        HttpModule,
+        HttpClientModule,
         JsonpModule
     ],
 
@@ -46,7 +37,6 @@ export let ABP_HTTP_PROVIDER = {
         AbpMultiTenancyService,
         AbpUserConfigurationService,
         AbpHttpConfiguration,
-        ABP_HTTP_PROVIDER,
         TokenService,
         UtilsService
     ]
