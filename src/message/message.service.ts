7454b17f6e6576cb9b8721c6b220ca416adfa4de
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-07-26 14:14:29 +0300

    upgraded to abp-web-resources v3.8

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -3,30 +3,30 @@
 @Injectable()
 export class MessageService {
 
-    info(message: string, title?: string): any {
-        return abp.message.info(message, title);
+    info(message: string, title?: string, isHtml?: boolean): any {
+        return abp.message.info(message, title, isHtml);
     }
 
-    success(message: string, title?: string): any {
-        return abp.message.success(message, title);
+    success(message: string, title?: string, isHtml?: boolean): any {
+        return abp.message.success(message, title, isHtml);
     }
 
-    warn(message: string, title?: string): any {
-        return abp.message.warn(message, title);
+    warn(message: string, title?: string, isHtml?: boolean): any {
+        return abp.message.warn(message, title, isHtml);
     }
 
-    error(message: string, title?: string): any {
-        return abp.message.error(message, title);
+    error(message: string, title?: string, isHtml?: boolean): any {
+        return abp.message.error(message, title, isHtml);
     }
 
     confirm(message: string, callback?: (result: boolean) => void): any;
-    confirm(message: string, title?: string, callback?: (result: boolean) => void): any;
+    confirm(message: string, title?: string, callback?: (result: boolean) => void, isHtml?: boolean): any;
 
-    confirm(message: string, titleOrCallBack?: string | ((result: boolean) => void), callback?: (result: boolean) => void): any {
+    confirm(message: string, titleOrCallBack?: string | ((result: boolean) => void), callback?: (result: boolean) => void, isHtml?: boolean): any {
         if (typeof titleOrCallBack == 'string') {
-            return abp.message.confirm(message, titleOrCallBack as string, callback);
+            return abp.message.confirm(message, titleOrCallBack as string, callback, isHtml);
         } else {
-            return abp.message.confirm(message, titleOrCallBack as ((result: boolean) => void));
+            return abp.message.confirm(message, titleOrCallBack as ((result: boolean) => void), isHtml);
         }
     }
 
