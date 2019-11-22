be3f65e0f4a21719c1723047367171209c26cf97
Musa Demir <48536631+demirmusa@users.noreply.github.com>
2019-11-22 16:45:25 +0300

    Update abpHttpInterceptor.ts

--- src/abpHttpInterceptor.ts
+++ src/abpHttpInterceptor.ts
@@ -257,14 +257,10 @@ export class AbpHttpInterceptor implements HttpInterceptor {
     }
 
     protected tryGetRefreshTokenService(): Observable<boolean> {
-        try {
-            var _refreshTokenService = this._injector.get(RefreshTokenService);
+        var _refreshTokenService = this._injector.get(RefreshTokenService, null);
 
-            if (_refreshTokenService) {
-                return _refreshTokenService.tryAuthWithRefreshToken();
-            }
-        } catch (error) {
-            return of(false);
+        if (_refreshTokenService) {
+            return _refreshTokenService.tryAuthWithRefreshToken();
         }
         return of(false);
     }
